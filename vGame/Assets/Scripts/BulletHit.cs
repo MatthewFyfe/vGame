e00582e3f0676eb09057d3bcafe252e23701fad2
Computer Science Guest 214 <zzz214@usask.ca>
2016-02-20 12:17:28 -0600

    Hit detection

---
+++ vGame/Assets/Scripts/BulletHit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletHit : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll) 
+	{	
+		if (coll.gameObject.tag != "Player") {
+			Destroy(coll.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
