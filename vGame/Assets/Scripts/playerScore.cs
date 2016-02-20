b3b693a7d11012cab65583aa0aa26d6b6a3d9667
MatthewFyfe <zzz110@PEON10.usask.ca>
2016-02-20 15:42:39 -0600

    Heads up Display
    updated scene to include points and clock

---
+++ vGame/Assets/Scripts/playerScore.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerScore : MonoBehaviour {
+
+	public int points = 0;
+	float survivalTime;
+	int minutes, seconds;
+
+	public Text scoreText;
+	public Text clockText;
+
+	// Use this for initialization
+	void Start () {
+		//track players game time and kills
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//track a live clock and scoreboard
+		scoreText.text = "Score: " + points.ToString();
+
+		//get current game time in minutes:seconds
+		survivalTime = Time.time;
+		minutes = (int)survivalTime / 60;
+		seconds = (int)survivalTime % 60;
+		clockText.text = minutes.ToString("00") + ':' + seconds.ToString("00");
+	}
+
+	void addPoints(int pointsToAdd)
+	{
+		points += pointsToAdd;
+	}
+
+	void gameOver()
+	{
+		//print a splash screen of points and time of match / game
+	}
+}
