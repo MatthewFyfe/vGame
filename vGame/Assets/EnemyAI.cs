b4405166b1b7ed7f3c3ffc35796a3e06140d7fcf
lmelnyk-va <zzz214@PEON9.usask.ca>
2016-02-20 17:33:59 -0600

    Enemy AI improvements

--- vGame/Assets/EnemyAI.cs
+++ vGame/Assets/EnemyAI.cs
@@ -10,66 +10,72 @@ public enum EnemyBehaviour {
 public class EnemyAI : MonoBehaviour {
 
 	public EnemyBehaviour behaviour = EnemyBehaviour.MoveToClosest;
+	public int preferredDistance = 10;
+	public int shootingDistance = 15;
+	public bool runsFromPlayer = false;
 
 	private GameObject Player;
 	private GameObject Skynet;
 	private float speed = 2.5f;
 	private Health health;
 
+	private Gun gun;
+
 	public int pointValue = 1;
+	private GameObject target;
 
 	// Use this for initialization
 	void Start () {
 		Player = GameObject.FindGameObjectWithTag("Player");
 		Skynet = GameObject.FindGameObjectWithTag("Skynet");
+		gun = transform.root.GetComponentInChildren<Gun>();
 		health = transform.root.GetComponentInChildren<Health>();
 		health.onDeathEvent += OnDeath;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		switch (behaviour) {
 		case EnemyBehaviour.MoveToPlayer:
-			MoveToPlayer ();
+			target = Player;
 			break;
 		case EnemyBehaviour.MoveToBase:
-			MoveToBase ();
+			target = Skynet;
 			break;
 		case EnemyBehaviour.MoveToClosest:
-			MoveToClosest ();
+			target = DetermineClosestUnit ();
 			break;
 		}
 
+		MoveToUnit ();
+		float distanceFromTarget = Vector3.Distance (transform.position, target.transform.position);
+		gun.isShooting = distanceFromTarget <= shootingDistance;
 	}
 
-	void MoveToPlayer(){
+	void MoveToUnit(){
 		// Set the rotation
-		float rotation = Mathf.Atan2( Player.transform.position.y - transform.position.y,
-		                             Player.transform.position.x - transform.position.x ) * Mathf.Rad2Deg - 90;
+		float rotation = Mathf.Atan2( target.transform.position.y - transform.position.y,
+		                             target.transform.position.x - transform.position.x ) * Mathf.Rad2Deg - 90;
 		transform.eulerAngles = new Vector3( 0, 0, rotation );
 
 		float distance = Time.deltaTime * speed;
-		transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, distance);
+		float distanceFromTarget = Vector3.Distance (transform.position, target.transform.position);
+		if (distanceFromTarget >= preferredDistance) {
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, distance);
+		} else if (runsFromPlayer) {
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, -distance);
+		}
 	}
 
-	void MoveToBase(){
-		// Set the rotation
-		float rotation = Mathf.Atan2( Skynet.transform.position.y - transform.position.y,
-		                             Skynet.transform.position.x - transform.position.x ) * Mathf.Rad2Deg - 90;
-		transform.eulerAngles = new Vector3( 0, 0, rotation );
-		
-		float distance = Time.deltaTime * speed;
-		transform.position = Vector3.MoveTowards(transform.position, Skynet.transform.position, distance);
-	}
-
-	void MoveToClosest(){
+	GameObject DetermineClosestUnit(){
 		var playerDistance = Vector3.Distance (transform.position, Player.transform.position);
 		var skynetDistance = Vector3.Distance (transform.position, Skynet.transform.position);
 
 		if (playerDistance > skynetDistance) {
-			MoveToBase ();
+			return Skynet;
 		} else {
-			MoveToPlayer();
+			return Player;
 		}
 	}
 
