aa0dfd7c285983961c7203aa4916fd10d40a859e
Jesse Webb <jwebb@vendasta.com>
2016-02-20 16:20:23 -0600

    add gitattributes to fix line endings in git

--- vGame/Assets/Scripts/moveVBot.cs
+++ vGame/Assets/Scripts/moveVBot.cs
@@ -3,14 +3,11 @@ using System.Collections;
 
 public class moveVBot : MonoBehaviour
 {
-	Vector3 VBot_position = new Vector3 ();
 	public float moveSpeed = 5.0f; //VBots moveSpeed coefficient
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		//check for controls and shit
-		VBot_position = new Vector3 ();
 		
 		float translationX = 0;
 		float translationY = 0;
