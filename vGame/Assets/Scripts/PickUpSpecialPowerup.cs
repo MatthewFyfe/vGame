71392cfe4766323d1e92eb4bc5ba43d64809fb09
lmelnyk-va <zzz214@PEON9.usask.ca>
2016-02-21 11:35:00 -0600

    More Power ups

---
+++ vGame/Assets/Scripts/PickUpSpecialPowerup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpSpecialPowerup : MonoBehaviour {
+	private float pickUpDistance = 1;
+	private GameObject player;
+
+	public float RPMMultIncrease;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, player.transform.position) <= pickUpDistance) {
+			Gun[] guns = player.GetComponentsInChildren<Gun>();
+			
+			foreach (Gun gun in guns) {
+				gun.RPMMultiplier += RPMMultIncrease;
+			}
+			
+			Destroy (gameObject);
+		}
+	}
+}
