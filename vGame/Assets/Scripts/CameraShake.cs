e56abf3ff1f99f3366add85b346d0c372a594320
Jason Prokop <jcp393@PEON11.usask.ca>
2016-02-21 14:51:39 -0600

    Add camera shaking to base and bomber deaths

---
+++ vGame/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	
+	// How long the object should shake for.
+	public float shakeTime = 0f;
+	
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.3f;
+	public float decreaseFactor = 1.0f;
+	
+	Vector3 originalPos;
+
+	void Start()
+	{
+		originalPos = transform.position;
+	}
+	
+	void Update()
+	{
+		if (shakeTime > 0)
+		{
+			Debug.Log(originalPos);
+			Debug.Log(Random.insideUnitSphere * shakeAmount);
+
+			transform.position = originalPos + Random.insideUnitSphere * shakeAmount * Random.Range(0.2f, 0.8f);
+			
+			shakeTime -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeTime = 0f;
+			transform.position = originalPos;
+		}
+	}
+}
